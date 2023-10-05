b7da3719f620f4b8e2160c0dba73fa28622eea96
Benjamin Sherman <benjamin@holyarmy.org>
2023-10-05 11:02:23 -0500

    feat: incoporate nvidia into main build simiar to zfs
    This should allow ucore image to be built with:
    - default-only
    - default+nvidia
    - default+zfs
    - default+nvidia+zfs
    
    hci should build on top of these

--- nvidia/post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -ouex pipefail
-
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
-
-semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
