c1243056f4c9560d618ef527853ef592c876da7b
Benjamin Sherman <benjamin@holyarmy.org>
2023-10-19 11:41:32 -0500

    feat: add generic fcos build with nvidia (#95)

---
+++ fedora-coreos/install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+#### PREPARE
+# enable testing repos if not enabled on testing stream
+if [[ "testing" == "${COREOS_VERSION}" ]]; then
+for REPO in $(ls /etc/yum.repos.d/fedora-updates-testing{,-modular}.repo); do
+  if [[ "$(grep enabled=1 ${REPO} > /dev/null; echo $?)" == "1" ]]; then
+    echo "enabling $REPO" &&
+    sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' ${REPO}
+  fi
+done
+fi
+
+# always disable cisco-open264 repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+
+#### INSTALL
+# inspect to see what RPMS we copied in
+find /tmp/rpms/
+
+## CONDITIONAL: install ZFS (and sanoid deps)
+if [[ "-zfs" == "${ZFS_TAG}" ]]; then
+    rpm-ostree install pv /tmp/rpms/zfs/*.rpm
+fi
+
+## CONDITIONAL: install NVIDIA
+if [[ "-nvidia" == "${NVIDIA_TAG}" ]]; then
+    # repo for nvidia rpms
+    curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/fedora-nvidia.repo
+
+    rpm-ostree install /tmp/rpms/nvidia/ublue-os-ucore-nvidia-*.rpm
+    sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' /etc/yum.repos.d/nvidia-container-toolkit.repo
+
+    rpm-ostree install \
+        /tmp/rpms/nvidia/kmod-nvidia-*.rpm \
+        nvidia-driver-cuda \
+        nvidia-container-toolkit
+fi
