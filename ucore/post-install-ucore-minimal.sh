56ec124f25404c57518053da31f9dd2c67f297ac
Benjamin Sherman <benjamin@holyarmy.org>
2024-08-24 14:39:56 -0500

    fix: gssproxy.service failing to start
    Fixes: #165
    
    This workaround should correct the gssproxy.service failure to start by
    using tmpfiles to create the missing directories in /var/lib and a
    service to restore the selinux labels on them.

--- ucore/post-install-ucore-minimal.sh
+++ ucore/post-install-ucore-minimal.sh
@@ -23,6 +23,8 @@ ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/bin/docker-compose
 systemctl disable docker.socket
 systemctl disable zincati.service
 
+systemctl enable gssproxy-workaround.service
+
 systemctl enable ucore-paths-provision.service
 systemctl enable rpm-ostreed-automatic.timer
 
