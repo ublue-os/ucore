a61fd1451822578dd21bb566608a64c3f6c82d77
Benjamin Sherman <benjamin@holyarmy.org>
2024-11-24 14:09:49 -0600

    comply with codady recommendation

--- ucore/install-ucore.sh
+++ ucore/install-ucore.sh
@@ -14,7 +14,7 @@ export IMAGE_NAME=ucore
 /ctx/packages.sh
 
 # install packages direct from github
-/ctx/github-release-install.sh trapexit/mergerfs fc${RELEASE}.x86_64
+/ctx/github-release-install.sh trapexit/mergerfs "fc${RELEASE}.x86_64"
 
 # tweak os-release
 sed -i '/^PRETTY_NAME/s/(uCore.*$/(uCore)"/' /usr/lib/os-release
