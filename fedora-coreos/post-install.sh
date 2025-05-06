49f23353cc6a6e8bbbc55ad1b8aa9611221c1b24
Benjamin Sherman <benjamin@holyarmy.org>
2025-05-05 20:00:14 -0500

    try other path?

--- fedora-coreos/post-install.sh
+++ fedora-coreos/post-install.sh
@@ -12,7 +12,7 @@ if [[ "-nvidia" == "${NVIDIA_TAG}" ]]; then
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
 
     rpm -qil ublue-os-ucore-nvidia
-    test -f /usr/share/selinux/packages/nvidia-container.pp
+    test -f /usr/share/ublue-os/usr/share/selinux/packages/nvidia-container.pp
 
-    semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
+    semodule --verbose --install /usr/share/ublue-os/usr/share/selinux/packages/nvidia-container.pp
 fi
