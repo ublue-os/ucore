7b6e6728081cfc17256304b3eb608c40de9fd672
Benjamin Sherman <benjamin@holyarmy.org>
2025-05-14 19:28:17 -0500

    refactor(ci): better handle curl retry
    this should address some of the issues seen with github rate limiting
    these builds.

--- ucore/install-ucore.sh
+++ ucore/install-ucore.sh
@@ -16,10 +16,10 @@ export IMAGE_NAME=ucore
 ## CONDITIONAL: ZFS support
 if [[ "-zfs" == "${ZFS_TAG}" ]]; then
     # cockpit plugin for ZFS management
-    curl --fail --retry 5 --retry-delay 5 --retry-all-errors -sSL -o /tmp/cockpit-zfs-manager-api.json \
+    curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/cockpit-zfs-manager-api.json \
         "https://api.github.com/repos/45Drives/cockpit-zfs-manager/releases/latest"
     CZM_TGZ_URL=$(jq -r .tarball_url /tmp/cockpit-zfs-manager-api.json)
-    curl -sSL -o /tmp/cockpit-zfs-manager.tar.gz "${CZM_TGZ_URL}"
+    curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/cockpit-zfs-manager.tar.gz "${CZM_TGZ_URL}"
 
     mkdir -p /tmp/cockpit-zfs-manager
     tar -zxvf /tmp/cockpit-zfs-manager.tar.gz -C /tmp/cockpit-zfs-manager --strip-components=1
@@ -27,7 +27,7 @@ if [[ "-zfs" == "${ZFS_TAG}" ]]; then
     mv /tmp/cockpit-zfs-manager/polkit-1/rules.d/* /usr/share/polkit-1/rules.d/
     mv /tmp/cockpit-zfs-manager/zfs /usr/share/cockpit
 
-    curl -sSL -o /tmp/cockpit-zfs-manager-font-fix.sh \
+    curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/cockpit-zfs-manager-font-fix.sh \
         https://raw.githubusercontent.com/45Drives/scripts/refs/heads/main/cockpit_font_fix/fix-cockpit.sh
     chmod +x /tmp/cockpit-zfs-manager-font-fix.sh
     /tmp/cockpit-zfs-manager-font-fix.sh
