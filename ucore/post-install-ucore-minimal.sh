44082a4a1ef62c9e5f00d5ae5914149d37edaf68
Benjamin Sherman <benjamin@holyarmy.org>
2025-06-12 15:39:25 -0500

    chore: include ZFS in all image builds

--- ucore/post-install-ucore-minimal.sh
+++ ucore/post-install-ucore-minimal.sh
@@ -2,11 +2,6 @@
 
 set -ouex pipefail
 
-## CONDITIONAL: post-install ZFS
-if [[ "-zfs" == "${ZFS_TAG}" ]]; then
-    echo "no post-install tasks for ZFS"
-fi
-
 ## CONDITIONAL: post-install NVIDIA
 if [[ "-nvidia" == "${NVIDIA_TAG}" ]]; then
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/nvidia-container-toolkit.repo
