da7bec8d252543dd66d40d2ff7638e33113459bf
Benjamin Sherman <benjamin@holyarmy.org>
2025-09-13 19:28:41 -0500

    feat: more secure Cockpit login (#286)

--- ucore/post-install-ucore-minimal.sh
+++ ucore/post-install-ucore-minimal.sh
@@ -27,9 +27,6 @@ systemctl enable rpm-ostreed-automatic.timer
 
 sed -i 's/#AutomaticUpdatePolicy.*/AutomaticUpdatePolicy=stage/' /etc/rpm-ostreed.conf
 
-# workaround to enable cockpit web logins
-rm /etc/ssh/sshd_config.d/40-disable-passwords.conf
-
 # workaround until distrobox patch for this makes it into repos
 ln -s  ../usr/share/zoneinfo/UTC /etc/localtime
 
