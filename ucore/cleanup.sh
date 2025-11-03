d3b417961b57a4e33329370ec7a66341d6b33285
Benjamin Sherman <benjamin@holyarmy.org>
2025-11-03 02:50:58 +0000

    don't allow /tmp cleanup to fail build

--- ucore/cleanup.sh
+++ ucore/cleanup.sh
@@ -3,7 +3,7 @@
 set -eoux pipefail
 
 find /boot/* -maxdepth 0 -exec rm -fr {} \; || true
-find /tmp/* -maxdepth 0 -type d \! -name rpms -exec rm -fr {} \;
+find /tmp/* -maxdepth 0 -type d \! -name rpms -exec rm -fr {} \; || true
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
 
