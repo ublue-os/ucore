f03a5bd5d72f9228e34c5fc0139aace4e36f1bf7
Benjamin Sherman <benjamin@holyarmy.org>
2025-11-11 10:31:38 -0600

    fix: pass zfs-import-scan by removing zfs from initramfs (#316)

--- ucore/install-ucore-minimal.sh
+++ ucore/install-ucore-minimal.sh
@@ -54,19 +54,21 @@ dnf -y install \
 # Ensure kernel packages can't be updated by other dnf operations
 dnf versionlock add kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
+# Regenerate initramfs, for new kernel; not including NVIDIA or ZFS kmods
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
+/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
 ## ALWAYS: install ZFS (and sanoid deps)
+# uCore does not support ZFS as rootfs, thus does not provide it in the initramfs
 dnf -y install /tmp/rpms/akmods-zfs/kmods/zfs/*.rpm /tmp/rpms/akmods-zfs/kmods/zfs/other/zfs-dracut-*.rpm
 # for some reason depmod ran automatically with zfs 2.1 but not with 2.2
 echo "Update modules.dep, etc..."
 depmod -a "${KERNEL_VERSION}"
 
-# Regenerate initramfs, for new kernel and zfs; not including NVIDIA kmod
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-
 ## CONDITIONAL: install NVIDIA
 if [[ "-nvidia" == "${NVIDIA_TAG}" ]]; then
+    # uCore expects NVIDIA drivers are able to hot load/unload, thus does not provide it in the initramfs
     # repo for nvidia rpms
     curl --fail --retry 15 --retry-all-errors -sSL https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/fedora-nvidia.repo
 
