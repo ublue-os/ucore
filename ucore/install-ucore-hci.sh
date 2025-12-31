64952bc8842b85028e34c5c75fe43d4855b5c925
Benjamin Sherman <benjamin@holyarmy.org>
2025-12-31 10:08:48 -0600

    refactor: directly use dnf install/swap rather than json file (#337)

--- ucore/install-ucore-hci.sh
+++ ucore/install-ucore-hci.sh
@@ -2,9 +2,13 @@
 
 set -ouex pipefail
 
-# install packages.json stuffs
-export IMAGE_NAME=ucore-hci
-/ctx/packages.sh
+# install packages
+dnf -y install \
+    cockpit-machines \
+    libvirt-client \
+    libvirt-daemon-kvm \
+    ublue-os-libvirt-workarounds \
+    virt-install
 
 # tweak os-release
 sed -i '/^PRETTY_NAME/s/(uCore.*$/(uCore HCI)"/' /usr/lib/os-release
--- ucore/install-ucore.sh
+++ ucore/install-ucore.sh
@@ -5,9 +5,43 @@ set -ouex pipefail
 ARCH="$(rpm -E %_arch)"
 RELEASE="$(rpm -E %fedora)"
 
-# install packages.json stuffs
-export IMAGE_NAME=ucore
-/ctx/packages.sh
+# install packages
+dnf -y swap nfs-utils-coreos nfs-utils
+dnf -y install \
+    NetworkManager-wifi \
+    atheros-firmware \
+    brcmfmac-firmware \
+    cockpit-storaged \
+    distrobox \
+    duperemove \
+    hdparm \
+    iwlegacy-firmware \
+    iwlwifi-dvm-firmware \
+    iwlwifi-mvm-firmware \
+    man-db \
+    mt7xxx-firmware \
+    nxpwireless-firmware \
+    pciutils \
+    pcp-zeroconf \
+    rclone \
+    realtek-firmware \
+    samba \
+    samba-usershares \
+    sanoid \
+    smartctl \
+    snapraid \
+    tiwilink-firmware \
+    usbutils \
+    xdg-dbus-proxy \
+    xdg-user-dirs
+
+# install packages direct from github
+if [[ "${RELEASE}" -ge "43" ]]; then
+  /ctx/github-release-install.sh trapexit/mergerfs "fc${RELEASE}.${ARCH}"
+elif [[ "${ARCH}" == "x86_64" ]]; then
+  # before F43, mergerfs only available for x86_64
+  /ctx/github-release-install.sh trapexit/mergerfs "fc${RELEASE}.x86_64"
+fi
 
 # cockpit plugin for ZFS management
 curl --fail --retry 15 --retry-all-errors -sSL -o /tmp/cockpit-zfs-manager-api.json \
@@ -27,14 +61,6 @@ chmod +x /tmp/cockpit-zfs-manager-font-fix.sh
 /tmp/cockpit-zfs-manager-font-fix.sh
 
 rm -rf /tmp/cockpit-zfs-manager*
-
-# install packages direct from github
-if [[ "${RELEASE}" -ge "43" ]]; then
-  /ctx/github-release-install.sh trapexit/mergerfs "fc${RELEASE}.${ARCH}"
-elif [[ "${ARCH}" == "x86_64" ]]; then
-  # before F43, mergerfs only available for x86_64
-  /ctx/github-release-install.sh trapexit/mergerfs "fc${RELEASE}.x86_64"
-fi
 
 # tweak os-release
 sed -i '/^PRETTY_NAME/s/(uCore.*$/(uCore)"/' /usr/lib/os-release
