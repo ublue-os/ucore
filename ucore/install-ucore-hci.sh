63bd558579c5168eabdba84f8b6bed3b83a71349
Benjamin Sherman <benjamin@holyarmy.org>
2026-04-01 11:19:02 -0500

    fix: disable ublue COPR repos before generic package installs (#370)

--- ucore/install-ucore-hci.sh
+++ ucore/install-ucore-hci.sh
@@ -3,11 +3,11 @@
 set -ouex pipefail
 
 # install packages
+dnf -y --enable-repo='copr:copr.fedorainfracloud.org:ublue-os:packages' install ublue-os-libvirt-workarounds
 dnf -y install \
     cockpit-machines \
     libvirt-client \
     libvirt-daemon-kvm \
-    ublue-os-libvirt-workarounds \
     virt-install
 
 # tweak os-release
